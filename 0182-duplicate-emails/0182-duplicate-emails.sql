df3fe838a9285b04b21eaf74cfaeaf7ad5aae544
Bae-Sangbin <121172763+Bae-Sangbin@users.noreply.github.com>
2023-09-05 08:31:35 +0900

    Time: 1053 ms (37.32%), Space: 0B (100.00%) - LeetHub

--- 0182-duplicate-emails/0182-duplicate-emails.sql
+++ 0182-duplicate-emails/0182-duplicate-emails.sql
@@ -1,5 +1,5 @@
-# Write your MySQL query statement below
-SELECT 
-email as Email 
-FROM PERSON 
-GROUP BY email HAVING count(email) > 1;  
+/* Write your PL/SQL query statement below */
+SELECT EMAIL
+FROM PERSON
+GROUP BY EMAIL
+HAVING COUNT(*) >= 2
