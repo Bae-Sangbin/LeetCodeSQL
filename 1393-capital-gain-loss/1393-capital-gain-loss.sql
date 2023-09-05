4688701a3505c9c2c2b5c779fefdd2cdfbea6fba
Bae-Sangbin <121172763+Bae-Sangbin@users.noreply.github.com>
2023-09-05 20:36:31 +0900

    Time: 1109 ms (49.15%), Space: 0B (100.00%) - LeetHub

--- 1393-capital-gain-loss/1393-capital-gain-loss.sql
+++ 1393-capital-gain-loss/1393-capital-gain-loss.sql
@@ -1,15 +1,9 @@
 /* Write your PL/SQL query statement below */
-
-SELECT  
-    STOCK_NAME,
-    SUM(ADJ_PRICE) AS capital_gain_loss
+SELECT STOCK_NAME, SUM(ADJ_PRICE) AS capital_gain_loss
 FROM (
-SELECT
-    STOCK_NAME,
-    OPERATION,
-    CASE WHEN OPERATION = 'Buy' THEN -PRICE
-    ELSE PRICE END AS ADJ_PRICE
-FROM STOCKS
-)
+    SELECT stock_name, 
+        CASE WHEN OPERATION = 'Buy' THEN -price
+             ELSE price END AS ADJ_PRICE
+    FROM STOCKS
+    )
 GROUP BY STOCK_NAME
-
