4e3357c6a5cc9a3a3dabd8da56ed24e628559b15
Bae-Sangbin <121172763+Bae-Sangbin@users.noreply.github.com>
2023-09-07 14:17:33 +0900

    Time: 1781 ms (11.66%), Space: 0B (100.00%) - LeetHub

--- 0262-trips-and-users/0262-trips-and-users.sql
+++ 0262-trips-and-users/0262-trips-and-users.sql
@@ -1,24 +1,12 @@
 /* Write your PL/SQL query statement below */
-
-SELECT
-      REQUEST_AT AS DAY,
-      ROUND(COUNT(P)/COUNT(ID),2) AS "Cancellation Rate"
-FROM (
-      SELECT 
-            ID, REQUEST_AT, 
-            CASE WHEN STATUS LIKE 'cancelled%' THEN 1
-            ELSE NULL END AS P
-      FROM TRIPS
-      WHERE REQUEST_AT BETWEEN '2013-10-01' AND '2013-10-03'
-      AND CLIENT_ID IN (
-            SELECT USERS_ID
-            FROM USERS
-            WHERE BANNED <> 'Yes'
-      )
-      AND DRIVER_ID IN (
-            SELECT USERS_ID
-            FROM USERS
-            WHERE BANNED <> 'Yes'
-      )
+SELECT 
+    REQUEST_AT AS DAY, 
+    ROUND(SUM(CASE WHEN STATUS LIKE 'cancelled%' THEN 1 ELSE 0 END) / COUNT(*), 2) AS "CANCELLATION RATE"
+FROM TRIPS T
+WHERE REQUEST_AT BETWEEN '2013-10-01' AND '2013-10-03'
+AND 'No' = ALL(
+    SELECT BANNED
+    FROM USERS
+    WHERE USERS_ID IN (T.CLIENT_ID, T.DRIVER_ID)
 )
 GROUP BY REQUEST_AT
