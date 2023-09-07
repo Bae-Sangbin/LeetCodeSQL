be1bea4e60beba8017e00cc7911949c7e8882f28
Bae-Sangbin <121172763+Bae-Sangbin@users.noreply.github.com>
2023-09-07 15:25:10 +0900

    Time: 4820 ms (5.06%), Space: 0B (100.00%) - LeetHub

--- 1068-product-sales-analysis-i/1068-product-sales-analysis-i.sql
+++ 1068-product-sales-analysis-i/1068-product-sales-analysis-i.sql
@@ -1,4 +1,4 @@
-# Write your MySQL query statement below
+/* Write your PL/SQL query statement below */
 SELECT P.PRODUCT_NAME, S.YEAR, S.PRICE
-FROM SALES S JOIN PRODUCT P
-ON S.PRODUCT_ID = P.PRODUCT_ID
+FROM SALES S, PRODUCT P 
+WHERE S.PRODUCT_ID = P.PRODUCT_ID
