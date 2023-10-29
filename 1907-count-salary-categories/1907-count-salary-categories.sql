ae85ec38135018f4ed060033d33ecb6affcb3c02
Bae-Sangbin <121172763+Bae-Sangbin@users.noreply.github.com>
2023-10-29 14:55:39 +0900

    Time: 1056 ms (89.63%), Space: 0B (100.00%) - LeetHub

--- 1907-count-salary-categories/1907-count-salary-categories.sql
+++ 1907-count-salary-categories/1907-count-salary-categories.sql
@@ -6,14 +6,13 @@ FROM Accounts
 WHERE income < 20000
 UNION
 SELECT
+    'Average Salary' AS category
+    , COUNT(income) AS accounts_count
+FROM Accounts
+WHERE income >= 20000 AND income <= 50000
+UNION
+SELECT
     'High Salary' AS category
     , COUNT(income) AS accounts_count
 FROM Accounts
 WHERE income > 50000
-UNION
-SELECT
-    'Average Salary' AS category
-    , COUNT(income) AS accounts_count
-FROM Accounts
-WHERE income >= 20000 AND income <= 50000;
-
