6c2bd7fccd199694e5bdba73e991a200ffafc2fe
Bae-Sangbin <121172763+Bae-Sangbin@users.noreply.github.com>
2023-10-29 14:53:23 +0900

    Time: 1574 ms (17.68%), Space: 0B (100.00%) - LeetHub

--- 1907-count-salary-categories/1907-count-salary-categories.sql
+++ 1907-count-salary-categories/1907-count-salary-categories.sql
@@ -1,6 +1,19 @@
 /* Write your PL/SQL query statement below */
-select 'Low Salary' as "category", count(account_id) as "accounts_count" from accounts where income < 20000
-union all 
-select 'Average Salary' as "category", count(account_id) as "accounts_count" from accounts where income between 20000 and 50000
-union all
-select 'High Salary' as "category", count(account_id) as "accounts_count" from accounts where income > 50000
+SELECT
+    'Low Salary' AS category
+    , COUNT(income) AS accounts_count
+FROM Accounts
+WHERE income < 20000
+UNION
+SELECT
+    'High Salary' AS category
+    , COUNT(income) AS accounts_count
+FROM Accounts
+WHERE income > 50000
+UNION
+SELECT
+    'Average Salary' AS category
+    , COUNT(income) AS accounts_count
+FROM Accounts
+WHERE income >= 20000 AND income <= 50000;
+
