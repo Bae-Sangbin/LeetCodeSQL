ae7d517651f1f43738936437a962ab808bf67ed0
Bae-Sangbin <121172763+Bae-Sangbin@users.noreply.github.com>
2023-10-29 14:51:58 +0900

    Time: 1320 ms (51.26%), Space: 0B (100.00%) - LeetHub

--- 1890-the-latest-login-in-2020/1890-the-latest-login-in-2020.sql
+++ 1890-the-latest-login-in-2020/1890-the-latest-login-in-2020.sql
@@ -1,5 +1,6 @@
-# Write your MySQL query statement below
+/* Write your PL/SQL query statement below */
 SELECT USER_ID, MAX(TIME_STAMP) AS LAST_STAMP
 FROM LOGINS
-WHERE YEAR(TIME_STAMP) = 2020
+WHERE EXTRACT(YEAR FROM TIME_STAMP) = 2020
 GROUP BY USER_ID
+;
